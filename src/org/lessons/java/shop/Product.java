83370796779f07dfce073c24cd436602db8202d1
AndreaCatapano <a.catapano96@gmail.com>
2025-06-28 13:54:43 +0200

    first commit

---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,11 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Product {
+    protected int serialNumber;
+    protected String name;
+    protected String brand;
+    protected BigDecimal price;
+    protected BigDecimal tax;
+}
