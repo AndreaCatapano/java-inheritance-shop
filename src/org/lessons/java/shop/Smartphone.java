cb96683a2a09c8350625306ebe9c74f23b15c968
AndreaCatapano <a.catapano96@gmail.com>
2025-06-28 14:43:47 +0200

    feat: created Smartphone Class

---
+++ src/org/lessons/java/shop/Smartphone.java
@@ -0,0 +1,27 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Smartphone extends Product {
+    private long IMEIcode;
+    private int memorySize;
+
+    Random rand = new Random();
+
+    public Smartphone(String name, String brand, BigDecimal price, int memorySize) {
+        super(name, brand, price);
+        this.IMEIcode = Math.abs(rand.nextLong() % (long) 1e15);
+        this.memorySize = memorySize;
+    }
+
+    // GETTER
+
+    public long getIMEIcode() {
+        return IMEIcode;
+    }
+
+    public int getMemorySize() {
+        return memorySize;
+    }
+}
